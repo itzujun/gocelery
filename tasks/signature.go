9a94f9987b683d83155284432c5a9c5c9eecccf9
cage <953761580@qq.com>
2019-02-25 16:05:50 +0800

    增加配置信息

--- tasks/signature.go
+++ tasks/signature.go
@@ -1,5 +1,11 @@
 package tasks
 
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"time"
+)
+
 type Arg struct {
 	Name  string      `bson:"name"`
 	Type  string      `bson:"type"`
@@ -11,4 +17,45 @@ type Headers map[string]interface{}
 
 func (h Headers) Set(key, value string) {
 	h[key] = value
+}
+
+func (h Headers) ForeachKey(handler func(key, val string) error) error {
+	for k, v := range h {
+		stringValue, ok := v.(string)
+		if !ok {
+			continue
+		}
+		if err := handler(k, stringValue); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+//new task signature
+type Signature struct {
+	UUID                 string
+	Name                 string
+	RouteKey             string
+	ETA                  *time.Time
+	GroupUUID            string
+	GroupTaskCount       int
+	Args                 []Arg
+	Immutable            bool
+	RetryCount           int
+	RetryTimeout         int
+	OnSuccess            []*Signature
+	OnError              []*Signature
+	ChordCallback        *Signature
+	BrokerMessageGroupId string
+}
+
+func NewSignature(name string, args []Arg) (*Signature, error) {
+	signatureID := uuid.New().String()
+	return &Signature{
+		UUID: fmt.Sprintf("task_%v", signatureID),
+		Name: name,
+		Args: args,
+	}, nil
+
 }
