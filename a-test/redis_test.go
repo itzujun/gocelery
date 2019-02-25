d4d9195e8b302791f4f9e7a7b5d550555cde9a76
cage <953761580@qq.com>
2019-02-25 21:35:43 +0800

    增加模块测试单元

---
+++ a-test/redis_test.go
@@ -0,0 +1,45 @@
+package a_test
+
+import (
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"github.com/itzujun/GoCelery/common"
+	"github.com/itzujun/GoCelery/config"
+	"testing"
+)
+
+func TestRedis(t *testing.T) {
+
+	cfg := &config.RedisConfig{
+		MaxIdle:                10,
+		MaxActive:              10,
+		IdleTimeout:            10,
+		Wait:                   true,
+		ReadTimeout:            10,
+		ConnectTimeout:         10,
+		DelayedTasksPollPeriod: 10,
+	}
+
+	rdconn := &common.RedisConnector{}
+
+	pool := rdconn.NewPool("", "127.0.0.1:6379", "", 1, cfg, nil)
+
+	conn, err := pool.Dial()
+	defer conn.Close()
+	if err != nil {
+		fmt.Println("redis连接error")
+	}
+	fmt.Println("连接OK")
+
+	_, err = conn.Do("SET", "name", "open_china")
+
+	if err != nil {
+		fmt.Println("error:", err.Error())
+	}
+	name, err := redis.String(conn.Do("GET", "name"))
+	if err != nil {
+		fmt.Println("error:", err.Error())
+	}
+	fmt.Println("name:", name)
+
+}
