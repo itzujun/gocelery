67048fc76d1d0bef336c268c000597be17df87b8
cage <953761580@qq.com>
2019-02-25 11:00:44 +0800

    增加errro信息

--- tasks/errors.go
+++ tasks/errors.go
@@ -1,1 +1,27 @@
 package tasks
+
+import (
+	"fmt"
+	"time"
+)
+
+type ErrRetryTaskLater struct {
+	name, msg string
+	retryIn   time.Duration
+}
+
+func RetryIn(e ErrRetryTaskLater) time.Duration {
+	return e.retryIn
+}
+
+func NewErrRetryTaskLater(msg string, retryIn time.Duration) ErrRetryTaskLater {
+	return ErrRetryTaskLater{msg: msg, retryIn: retryIn}
+}
+
+func (e ErrRetryTaskLater) Error() string {
+	return fmt.Sprintf("Task error %s will retry in %s", e.msg, e.retryIn)
+}
+
+type Retriable interface {
+	RetryIn() time.Duration
+}
